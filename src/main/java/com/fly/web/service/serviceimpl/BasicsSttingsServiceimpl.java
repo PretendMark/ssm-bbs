f105d582b1ebcebf5ec9b977d2e631929096af83
YuLF <1650056068@qq.com>
2020-11-02 22:34:55 +0800

    增加通过用户id 查询用户信息

--- src/main/java/com/fly/web/service/serviceimpl/BasicsSttingsServiceimpl.java
+++ src/main/java/com/fly/web/service/serviceimpl/BasicsSttingsServiceimpl.java
@@ -2,6 +2,7 @@ package com.fly.web.service.serviceimpl;
 
 import com.fly.web.dao.BasicsSttingsDao;
 import com.fly.web.pojo.ProvincialAndCityDO;
+import com.fly.web.pojo.UserDO;
 import com.fly.web.service.BasicsSttingsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -28,5 +29,12 @@ public class BasicsSttingsServiceimpl implements BasicsSttingsService {
     @Override
     public boolean saveUserPicture(String userEmailName, String userPictureName) {
         return basicsSttingsDao.saveUserPicture(userEmailName,userPictureName) > 0 ? true : false;
+    }
+
+    @Override
+    public Boolean updateUserInfo(UserDO user) {
+        //省市转换存储
+        user.setUserCity(user.getProvincial()+"-"+user.getCity());
+        return basicsSttingsDao.updateUserInfo(user) > 0 ? true : false;
     }
 }
