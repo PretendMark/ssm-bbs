5a604325c9bc793262ded5b03416d55ac1b19f95
YuLF <1650056068@qq.com>
2020-11-02 22:34:26 +0800

    修改不同方法验证实体 通过@Validation注解的接口分组方式 验证

--- src/main/java/com/fly/web/pojo/UserDO.java
+++ src/main/java/com/fly/web/pojo/UserDO.java
@@ -6,33 +6,38 @@ import com.fly.web.file.UserFileUploadAccessor;
 import com.fly.web.util.DateConverter;
 import com.fly.web.util.PathHandler;
 import com.mysql.jdbc.StringUtils;
+import com.sun.istack.internal.NotNull;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 import java.util.Date;
 
 /**
- * 注册
+ * 用户数据效验类
  */
 public class UserDO implements Serializable {
-    private Integer uid;
-    @Email( message = "您的邮箱不正确!" )
+    @NotBlank( groups = {updatePersonalDataGroup.class}, message = "您的用户id不能为空!" )
+    @Length( groups = {updatePersonalDataGroup.class}, min = 1, max = 9, message = "用户id错误!")
+    private String uid;
+    @NotBlank( groups = {registerGroup.class}, message = "您的邮箱不能为空!" )
+    @Email( groups = {registerGroup.class}, message = "您的邮箱不正确!" )
     private String userEmail;       /* 用户邮箱 */
-    @NotEmpty
-    @Length( min = 1, max = 10, message = "您的名字长度不正确!" )
-    private String userName = "asdadasd";        /* 用户名 */
+    @NotBlank( groups = {registerGroup.class, updatePersonalDataGroup.class}, message = "您的名称不能为空!" )
+    @Length( groups = {registerGroup.class, updatePersonalDataGroup.class}, min = 1, max = 10, message = "您的名字长度不正确!" )
+    private String userName;        /* 用户名 */
 
-
-    @NotEmpty
-    @Length( min = 6, max = 16, message = "您的密码长度不正确!" )
+    @NotBlank( groups = {registerGroup.class} , message = "您的密码不能为空!")
+    @Length( groups = {registerGroup.class},  min = 6, max = 16, message = "您的密码长度不正确!" )
     private String userPassword;    /* 用户密码 */
 
-
-    @Length( min = 4, max = 5, message = "您的验证码长度不正确!" )
+    @NotBlank( groups = {registerGroup.class} )
+    @Length( groups = {registerGroup.class},  min = 4, max = 5, message = "您的验证码长度不正确!" )
     private String userVerifyCode;  /* 用户提交的验证码 */
-    @Length( min = 16, max = 16, message = "您的Token长度不正确!" )
+    @NotBlank( groups = {registerGroup.class}, message = "您的Token不能为空!" )
+    @Length( groups = {registerGroup.class},  min = 15, max = 16, message = "您的Token长度不正确!" )
     private String userToken;       /* 用户密钥 */
     private String	userPasswordSalt;
     private String	userPasswordSha1;
@@ -43,17 +48,34 @@ public class UserDO implements Serializable {
     private Integer userGold;
     private String	userRegDate;
     private String	userCity;       /* 用户省市 */
+    @NotEmpty( groups = {updatePersonalDataGroup.class} ,message = "您的省份效验错误，不能为空")
+    @Length( groups = {updatePersonalDataGroup.class}, min = 3, max = 10, message = "您的省份效验错误，不能为空，最小3字符，最大10个字符")
     private String	provincial;     /* 用户省 */
+    @NotBlank( groups = {updatePersonalDataGroup.class}, message = "您所在的市效验失败，不能为空")
+    @Length( groups = {updatePersonalDataGroup.class}, min = 2, max = 10, message = "您所在的市效验失败，最小2字符，最大10字符")
     private String	city;           /* 用户市 */
+    @NotBlank( groups = {updatePersonalDataGroup.class}, message = "性别不能为空")
+    @Length( groups = {updatePersonalDataGroup.class}, max = 1, message = "您的性别长度不对")
     private String	userGender;
+
     private String	userPicture;
+
+    @Length( groups = {updatePersonalDataGroup.class}, max = 255, message = "您的签名超出最大长度255")
     private String	userSign;
+
     private String	userAuth;
 
     public UserDO()
     {
     }
 
+    /**
+     * 定义接口，用于指明在什么情况下，使用对应的验证规则
+     */
+    //验证修改个人资料 分组接口
+    public interface updatePersonalDataGroup {}
+    //验证注册资料接口
+    public interface registerGroup{}
 
     public void setPassword( String userPasswordSalt, String userPasswordSha1, String userPasswordMd5 )
     {
@@ -147,13 +169,13 @@ public class UserDO implements Serializable {
     }
 
 
-    public Integer getUid()
+    public String getUid()
     {
         return(uid);
     }
 
 
-    public void setUid( Integer uid )
+    public void setUid( String uid )
     {
         this.uid = uid;
     }
@@ -288,12 +310,8 @@ public class UserDO implements Serializable {
 
     public void setUserGender( String userGender )
     {
-        if ( !StringUtils.isNullOrEmpty(userGender)  && userGender.equals( "1" ) )
-        {
-            this.userGender = "女";
-        } else { this.userGender = "男"; }
-        /* 如果用户默认没有上传头像因为Mybatis 如果数据字段为空，不会执行该字段的set方法，但是这个set性别是一定会执行的 */
-        System.out.println("照片为空嘛？"+this.userPicture);
+        this.userGender = userGender;
+        /* 用户注册时设置用户默认头像：如果用户默认没有上传头像因为Mybatis 如果数据字段为空，不会执行该字段的set方法，但是这个set性别是一定会执行的 */
         if (StringUtils.isNullOrEmpty(this.userPicture) )
         {
             this.userPicture = PathHandler.getDefaultPicture( this.userGender );
